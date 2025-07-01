30b568a65687edd353e39e12d60c5d79a5565324
end-4 <97237370+end-4@users.noreply.github.com>
2025-07-01 21:10:01 +0200

    sbctl: more signing

--- install-finalize.sh
+++ install-finalize.sh
@@ -7,3 +7,9 @@ for kernel in /boot/vmlinuz-*; do
     sudo sbctl sign -s "$kernel"
 done
 sudo sbctl sign -s /boot/efi/EFI/Boot/BOOTx64.EFI
+sudo sbctl verify | sed -E 's|^.* (/.+) is not signed$|sudo sbctl sign -s "\1"|e'
+sudo sbctl status
+
+echo '[Action required] Go to firmware settings and enable secure boot and you'\''re done!'
+read -rp "[[ Press Enter to reboot, or Ctrl+C to cancel ]] " dummyVar
+systemctl reboot --firmware-setup
