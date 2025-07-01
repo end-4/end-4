f1073a1e84d8f1f3f5613b2ad979c441c9731e43
end-4 <97237370+end-4@users.noreply.github.com>
2025-07-01 20:56:11 +0200

    sbctl: sudo

--- install-eos.sh
+++ install-eos.sh
@@ -18,7 +18,9 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
-sudo echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n" | sudo tee -a /etc/pacman.conf
+if ! grep -q '^\[chaotic-aur\]' /etc/pacman.conf; then
+    echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n" | sudo tee -a /etc/pacman.conf
+fi
 
 echo '[[ Installing stuff ]]'
 yay -Syyu --needed --noconfirm \
--- install-finalize.sh
+++ install-finalize.sh
@@ -3,6 +3,7 @@
 sudo sbctl create-keys
 sudo sbctl enroll-keys -m
 for kernel in /boot/vmlinuz-*; do
+    echo "$kernel"
     sudo sbctl sign -s "$kernel"
 done
-sbctl sign -s /boot/efi/EFI/Boot/BOOTx64.EFI
+sudo sbctl sign -s /boot/efi/EFI/Boot/BOOTx64.EFI
